96a4b7549be4129097de3b01bdd0b1b5e19d3dfc
BytteL <lettybedard@gmail.com>
2020-01-25 16:58:21 -0500

    entered q & a data

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# assignment4
-Assignment 4 - Quiz
+# javascript-quiz
+Assignment 4, written by Letty Bedard
+
+## Assignment Description
+
+Create an application that generates a random password based on user-selected criteria. This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code.
+
+The user will be prompted to choose from the following password criteria:
+
+ - Length (must be between 8 and 128 characters)
+ - Character type:
+   - Special characters
+   - Numeric characters
+   - Lowercase characters
+   - Uppercase characters
+
+The application should validate user input and ensure that at least one character type is selected.
+Once all prompts are answered, the user will be presented with a password matching the answered prompts. Displaying the generated password in an alert is acceptable, but attempt to write the password to the page instead.
+
+(As a bonus, the user should also have the option to click a button to copy the password to their clipboard.)
+
+## Links
+
+[github source](https://github.com/yttel/javascript-quiz)
+
+[deployed site](https://yttel.github.io/javascript-quiz/)
+
+## Screenshots
+
+![Initial screen]()
+
+![During gameplay]()
+
+![End of game screen]()
+
+![High score list]()
--- script.js
+++ script.js
@@ -3,42 +3,64 @@
 $(document).ready(function(){
   
   //  DECLARATIONS 
-  const questions = [{
-    incorrectChoices: ["a1", "a2", "a3"],
-    correctAnswer: "a4",
-    questionText: "q1"
+  const questions = [{ 
+    incorrectChoices: ["<javascript>", "<js>", "<scripting>"],
+    correctAnswer: "<script>",
+    questionText: "Inside which HTML element do we put the JavaScript?"
+  },{ 
+    incorrectChoices: ["The <head>", "The <body>", "Neither one is correct."],
+    correctAnswer: "Both are correct.",
+    questionText: "Where is the correct place to insert a JavaScript?"
   },{
-    incorrectChoices: ["a1", "a2", "a3"],
-    correctAnswer: "a4",
-    questionText: "q2"
+    incorrectChoices: ["True."],
+    correctAnswer: "False.",
+    questionText: "An external JavaScript file must contain the <script> tag."
   },{
-    incorrectChoices: ["a1", "a2", "a3"],
-    correctAnswer: "a4",
-    questionText: "q3"
+    incorrectChoices: ["function:myFunction()", "funciton = myFunction()"],
+    correctAnswer: "funciton myFunction()",
+    questionText: "How do you create a function in JavaScript?"
   },{
-    incorrectChoices: ["a1", "a2", "a3"],
-    correctAnswer: "a4",
-    questionText: "q4"
+    incorrectChoices: ["if i = 5", "if i == 5 then", "if i = 5 then"],
+    correctAnswer: "if (i == 5)",
+    questionText: "How do you write an IF statement in JavaScript?"
   },{
-    incorrectChoices: ["a1", "a2", "a3"],
-    correctAnswer: "a4",
-    questionText: "q5"
+    incorrectChoices: ["True."],
+    correctAnswer: "False.",
+    questionText: "Java is just shorthand for JavaScript."
+  },{
+    incorrectChoices: ["boolean", "string", "array"],
+    correctAnswer: "integer",
+    questionText: "Which of these is NOT a valid JavaScript data type?"
+  },{
+    incorrectChoices: ["while i = 1 to 10", "while (i <= 10; i++)"],
+    correctAnswer: "while (i <= 10)",
+    questionText: "How does a WHILE loop start?"
+  },{
+    incorrectChoices: ["for (i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5"],
+    correctAnswer: "for (i = 0; i <= 5; i++)",
+    questionText: "How does a FOR loop start?"
+  },{
+    incorrectChoices: ["let ages = 3, 7, 8, 36, 37", "let ages = {3, 7, 8, 36, 37}", "let ages = (3, 7, 8, 36, 37)"],
+    correctAnswer: "let ages = [3, 7, 8, 36, 37]",
+    questionText: "What is the correct way to write a JavaScript array?"
+
   }];
+
+  let timeInterval;
 
   const timer = {
     theTime: 75,
     start: function (){
-      let timeInterval = setInterval(function() {
-          let timeLeft = this.theTime;
-          if (timeLeft >= 0){
-            time = timeLeft;
-            timeLeft--;
-            timer.start(timeInterval);
-            
+      timeInterval = setInterval(function() {
+          //console.log(this);
+          console.log(timer.theTime);
+          if (timer.theTime >= 0){
+            timer.theTime--;            
+            time.text(timer.theTime);
           }
-          if (this.theTime <= 0){
-            timeLeft = 0;
-            time = 0;
+          if (timer.theTime <= 0){
+            timer.theTime = 0;
+            time.text(0);
             clearInterval(timeInterval);
           }
         }, 1000);
@@ -63,7 +85,7 @@ $(document).ready(function(){
     middlePart.children().each(function() {
       //console.log(this);
       //console.log(quiz);
-      console.log($(this).hasClass("isHidden"));
+      //console.log($(this).hasClass("isHidden"));
       if(!($(this).hasClass("isHidden"))){
         $(this).addClass("isHidden");
       }
@@ -77,13 +99,13 @@ $(document).ready(function(){
 
   function showHighScores(){ //show high scores pane
     showThis("#hiScorePane");
+    scoreList.empty();
     let scores = localStorage.getItem("hiScoreList");
     // render the list of high scores
     if (scores) { //there is a hs list
-      scoreList.empty(); //kill existing children
       for (let i=0; i < scores.length; i++){
-       let nextScore = $("<li>"); 
-       nextScore.text(`${scores[i][0]}   ${scores[i][1]}`);
+       let nextScore = $("<li>").text(`${scores[i].score}   ${scores[i].name}`);
+       console.log(nextScore);
        scoreList.append(nextScore);
       } 
     }
@@ -93,40 +115,39 @@ $(document).ready(function(){
   }
 
   function showInstructions(){ //shows the instruction pane
+    time.text("75");
     showThis("#instructionPane");
   }
 
   function addHighScore(num, init){
     let scores = localStorage.getItem("hiScoreList");
-    // add the score num with init(ials) to the correct place in the high scores list
-    if (scores) { //there is already a list
-      for (let i=scores.length; i > 0; i--){
-        if (num < scores[i][0]){
-          scores.splice(i+1, 0, [num, init]);
-          break;
-        }
-      }
+    if (scores === null){
+      scores = [];
     }
-    else{ // there isn't, so make a new one
-      scores = [num, init];  
+    else {
+      scores = scores.parse();
     }
-    localStorage.setItem("hiScoreList", scores);
-  }
+    console.log(`scores: ${scores}`);
+    
+    scores.unshift({
+      score: num, 
+      name: init
+    });
 
-  function shuffleQuestions(question){ //shuffles questions
-    // loop that randomly grabs a question from the array and pushes it into a new one, also shuffling the order of the answers
-    let unshuffled = questions;
-    let shuffled = [];
-      for (let i = unshuffled.length; i > 0; --i){
-        shuffled.push(unshuffled[Math.floor(Math.random)*i]);
-      }
-    questions = shuffled;
+    console.log(`scores: ${scores}`);
+    
+    //sort scores highest to lowest
+
+    localStorage.setItem("hiScoreList", scores);
+    console.log("check local storage score list");
   }
 
   function flashResult(ansBool){
-    console.log(ansBool);
     let ansText = $("#resultText");
-    if (ansBool){ //correct answer
+    ansText.text("")
+            .css("opacity", 100);
+
+    if (ansBool === "true"){ //correct answer
       console.log("right");
       ansText.css("color", "green")
               .text("CORRECT!")
@@ -139,70 +160,98 @@ $(document).ready(function(){
               .text("WRONG!")
               .fadeTo("slow", 0);
     }
-
   }
 
   function playTheGame(){ //play the game
     // pick 5 random questions from the array and display
+    timer.theTime = 75;
     showThis("#quizSection");
     let thisRoundQ = Array.from(questions);
-    timer.start();
-    // for (let i = 0; i < 5; i++){ //grab 5 random questions
-    //   if (timer.theTime > 0){
-    //     let randomNum = Math.floor(Math.random()*(thisRoundQ.length));
-    //     //console.log(`randNum: ${randomNum}`);
-    //     let aQuestion = thisRoundQ.splice(randomNum, 1);
-    //     console.log(`thisRoundQ: ${thisRoundQ}`);
-    //     //console.log(aQuestion);
-    //     askAQuestion(aQuestion[0]);
-    //   }
-    //   else {
-    //     break;
-    //   }
-    // }
+    
+    //shuffle questions
+    thisRoundQ.sort(function (){return (0.5-Math.random())});
 
-    let randomNum = Math.floor(Math.random()*(thisRoundQ.length));
-    //console.log(`randNum: ${randomNum}`);
-    let aQuestion = thisRoundQ.splice(randomNum, 1);
-    //console.log(`thisRoundQ: ${thisRoundQ}`);
-    //console.log(aQuestion);
-    askAQuestion(aQuestion[0]);
- 
+    top5Questions = thisRoundQ.splice(0, 5);
+
+    timer.start();
+
+    askAQuestion(top5Questions);
   }
 
-  function askAQuestion(singleQuest){
-
+  function askAQuestion(questArr){ //asks the first question in the array (by removal)
     quiz.empty(); //kill existing question
-
-    quiz.append($("<div>")
-                .addClass("questText")
-                .text(singleQuest.questionText));
+    if (questArr[0] && timer.theTime > 0){ //there is at least one question and time left
+      let singleQuest = questArr.shift();
+      quiz.append($("<div>")
+                  .addClass("questText")
+                 .text(singleQuest.questionText));
     
-    //console.log(singleQuest);
-    let answerOptions = Array.from(singleQuest.incorrectChoices);
-    answerOptions.push(singleQuest.correctAnswer);
-    //console.log(answerOptions);
+      //console.log(singleQuest);
+      let answerOptions = Array.from(singleQuest.incorrectChoices);
+      answerOptions.push(singleQuest.correctAnswer);
+      //console.log(answerOptions);
 
-    // shuffle array
-    answerOptions.sort(function (){return (0.5-Math.random())});
+      // shuffle array
+      answerOptions.sort(function (){return (0.5-Math.random())});
 
-    answerOptions.forEach(function(thisAns){
-      //console.log(`thisAns: ${thisAns}`)
-      quiz.append($("<li>")
-                  .addClass("answerText")
-                  .text(thisAns)
-                  .attr("data-frog", (singleQuest.correctAnswer === thisAns)));
-    });
+      answerOptions.forEach(function(thisAns){
+        //console.log(`thisAns: ${thisAns}`)
+        quiz.append($("<li>")
+                    .addClass("answerText")
+                    .text(thisAns)
+                    .attr("data-frog", (singleQuest.correctAnswer === thisAns)));
+      });
   
-    $("li").on("click", function(){ //one of the answers is clicked
+      $("li").on("click", function(){ //one of the answers is clicked
       //console.log($(this));
-      flashResult($(this).attr("data-frog"));
+      //flashResult($(this).attr("data-frog"));
+        let ansText = $("#resultText");
+        ansText.text("")
+                .css("opacity", 100);
+        
+        //console.log($(this).attr("data-frog")==="true");        
+        if ($(this).attr("data-frog")==="true"){ //correct answer
+          console.log("right");
+          ansText.css("color", "green")
+                  .text("CORRECT!")
+                  .fadeTo("slow", 0);
+        }
+        else { //wrong answer
+          console.log("wrong");
+          timer.theTime -= 10;
+          ansText.css("color", "red")
+                  .text("WRONG!")
+                  .fadeTo("slow", 0);
+        }
+        // after they answer, ask next question
+        askAQuestion(questArr); 
+      });
+    }
+    else { // all questions were asked OR no time left
+      quizDone();
+    }
+  }
+
+  function quizDone(){
+    clearInterval(timeInterval);
+    quiz.empty();
+    showThis("#gameDone");
+    // show game over, grab initials
+    $("#endScore").text = timer.theTime;
+
+    $("#add").on("click", function (){
+      let initVal = $("#init").val();
+      console.log(`init: ${initVal}`);
+      addHighScore(timer.theTime, initVal);
+      showHighScores();
     });
+    
   }
 
   function resetScores(){
     //reset high scores (remove from local memory)
     localStorage.removeItem("hiScoreList");
+    showHighScores();
   }
 
   //  EVENT LISTENERS
